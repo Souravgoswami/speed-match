5c90c06c01b826473c3ec6583ad9b935bfa77d38
Sourav Goswami <37468605+Souravgoswami@users.noreply.github.com>
2018-11-05 03:56:00 +0530

    Add files via upload

---
+++ main.rb
@@ -0,0 +1,410 @@
+#!/usr/bin/ruby -W0
+require 'ruby2d'
+STDOUT.sync = true
+
+module Ruby2D
+	def change_colour=(colour)
+		opacity_ = self.opacity
+		self.color = colour
+		self.opacity = opacity_
+	end
+
+	def r=(r_val) self.color = [r_val, self.g, self.b, self.opacity] end
+	def g=(g_val) self.color = [self.r, g_val, self.b, self.opacity] end
+	def b=(b_val) self.color = [self.r, self.g, b_val, self.opacity] end
+end
+
+def main()
+	$width, $height, $fps = 640, 480, 50
+
+	$generate_colour = -> {
+		colour = ''
+		6.times do colour += [('a'..'f').to_a.sample, ('0'..'9').to_a.sample].sample end
+		"##{colour}"
+	}
+
+	available_images = %w(3d_ball 5petals_filled balls bouquet bulb circle circles1 circles2 diamond_hex diamond dice
+							hearts hexagon petals polygon ring ring2 rubik1 rubik2 star triangle triangle2)
+	selected_images = available_images.sample(rand(1..2))
+
+	$generate_image = ->(image_set) {
+		image = Image.new("shapes/#{image_set.sample}.png")
+		image.x, image.y, image.opacity = $width, $height/2 -image.height/1.5, 0.5
+		image
+	}
+
+	$control = ->(object, type='reduce', val=0.05, threshold=0.65, max=1) {
+		object.opacity -= val if object.opacity > threshold and type == 'reduce'
+		object.opacity += val if object.opacity < max and type != 'reduce'
+	}
+
+	$t = ->(format='%s') { Time.new.strftime(format) }
+
+	three = Text.new '3', font: 'fonts/Aller_Lt.ttf', size: 30, z: 5
+	three.x, three.y, three.opacity = $width/2 - three.width/2, $height/2 - three.height/2, 0
+
+	two = Text.new '2', font: 'fonts/Aller_Lt.ttf', size: 30, z: 5
+	two.x, two.y, two.opacity = $width/2 - two.width/2, $height/2 - two.height/2, 0
+
+	one = Text.new '1', font: 'fonts/Aller_Lt.ttf', size: 30, z: 5
+	one.x, one.y, one.opacity = $width/2 - one.width/2, $height/2 - one.height/2, 0
+
+	set title: 'Speed Match', width: $width, height: $height, resizable: true, background: $generate_colour.call, fps_cap: $fps
+	bg = Image.new "images/bg.jpg", width: $width, height: $height, z: -10
+
+	started = false
+
+	button_yes_touched, button_yes_pressed = false, false
+	button_yes = Rectangle.new width: $width/2 - 5, height: $height/5, x: 1
+	button_yes.y = $height - button_yes.height - 1
+
+	yes_text = Text.new 'YES', font: 'fonts/Aller_Lt.ttf', size: button_yes.height/1.5, color: 'teal'
+	yes_text.x, yes_text.y = button_yes.x + button_yes.width/2 - yes_text.width/2, button_yes.y + button_yes.height/2 - yes_text.height/2
+
+	button_no_touched, button_no_pressed = false, false
+	button_no = Rectangle.new width: $width/2 - 5, height: $height/5
+	button_no.x, button_no.y = button_yes.x + button_yes.width + 8, $height - button_no.height - 1
+
+	no_text = Text.new 'NO', font: 'fonts/Aller_Lt.ttf', size: button_no.height/1.5, color: 'teal'
+	no_text.x, no_text.y = button_no.x + button_no.width/2 - no_text.width/2, button_no.y + button_no.height/2 - no_text.height/2
+
+	squares, squares_speed = [], []
+
+	100.times do
+		size = rand(6..10)
+		square_ = Image.new 'shapes/1pixel_square.jpg', width: size, height: size, color: $generate_colour.call, z: -1
+		square_.x, square_.y, square_.opacity = rand(0..$width - square_.width), rand(0..$height - square_.height), rand(0.2..0.5)
+		squares << square_
+		squares_speed << rand(1.0..4.0)
+	end
+
+	instruction_text_touched = false
+	instruction_text = Text.new 'Does this card match the previous card?', font: 'fonts/Aller_Lt.ttf', size: 22
+	instruction_text.x, instruction_text.y = $width/2 - instruction_text.width/2, 50
+
+	items = []
+	items_touched = false
+	items << $generate_image.call(selected_images) if items.length < 1
+
+	sound_correct = Sound.new 'sounds/131662__bertrof__game-sound-correct-v2.wav'
+	sound_wrong = Sound.new 'sounds/131657__bertrof__game-sound-wrong.wav'
+
+	correct = Image.new 'images/correct.png', width: $width/10, height: $width/10
+	correct.x, correct.y, correct.opacity = $width/2 - correct.width/2, button_yes.y - correct.height - 5, 0
+
+	wrong = Image.new 'images/wrong.png', width: $width/10, height: $width/10
+	wrong.x, wrong.y, wrong.opacity = $width/2 - wrong.width/2, button_yes.y - wrong.height - 5, 0
+
+	pause = Image.new 'images/pause.png', width: $width/20, height: $width/25, z: 12
+	pausetext = Text.new "Play/Pause\t", x: pause.x + pause.width, y: pause.y, color: 'blue', z: 12
+	pausebox_touched, pause_clicked, pausetext.opacity = false, false, 0
+	pausebox = Rectangle.new width: pause.width - 3, height: pause.height - 2, x: 1, y: 1, z: 11
+
+	pause_var = 0
+	pause_blur = Rectangle.new x: 0, y: 0, width: $width, height: $height, color: 'black', z: 10
+	pause_blur.opacity = 0.7
+
+	resume_text_touched = false
+	resume_text = Text.new 'Play!', font: 'fonts/Aller_Lt.ttf', size: 100, z: pause_blur.z
+	resume_text.x, resume_text.y = pause_blur.x + pause_blur.width/2 - resume_text.width/2, pause_blur.y + pause_blur.height/2
+
+	resume_button_touched = false
+	resume_button = Image.new 'images/play_button.png', z: resume_text.z
+	resume_button.x, resume_button.y = pause_blur.x + pause_blur.width/2 - resume_button.width/2, resume_text.y - resume_button.height
+
+	about_button_touched = false
+	about_button = Image.new 'images/bulb.png', z: resume_button.z
+	about_button.x, about_button.y = $width - about_button.width - 5, button_no.y - about_button.height - 5
+
+	power_touched = false
+	power_button = Image.new 'images/power.png', z: resume_button.z, x: 5, y: about_button.y
+
+	play_button2_touched = false
+	play_button2 = Image.new 'images/play_button_64x64.png', z: resume_button.z, x: power_button.x, y: pausebox.y + pausebox.height + 5
+
+	restart_touched = false
+	restart_button = Image.new 'images/restart.png', x: about_button.x, y: play_button2.y, z: resume_button.z
+
+	score = 0
+
+	score_touched = false
+	score_text = Text.new "\tSCORE\t\t#{score}\t", font: 'fonts/Aller_Lt.ttf', size: 15, color: 'blue'
+	score_text.x = $width - score_text.width - 5
+	score_box = Rectangle.new x: score_text.x, y: score_text.y + 1, width: score_text.width, height: score_text.height, z: -1
+
+	time_touched = false
+	time_text = Text.new "\tTIME\t\t#{45}\t", font: 'fonts/Aller_Lt.ttf', size: 15, color: 'blue'
+	time_text.x = score_text.x - time_text.width - 5
+	time_box = Rectangle.new x: time_text.x, y: time_text.y + 1, width: time_text.width, height: time_text.height, z: -1
+
+	prev_item, next_item = '', ''
+
+	on :key_held do |k|
+		button_yes_touched = true if %w(left a 1 j).include?(k.key)
+		button_no_touched = true if %w(right d 3 ;).include?(k.key)
+	end
+
+	on :key_down do |k|
+		button_yes_pressed = true if %w(left a 1 j).include?(k.key) or k.key.match(/4/)
+		button_no_pressed = true if %w(right d 3 ;).include?(k.key) or k.key.match(/6/)
+		pause_var += 1 if %w(space escape f).include?(k.key)
+	end
+
+	on :key_up do |k|
+		button_yes_touched = false if k.key == 'left'
+		button_no_touched = false if k.key == 'right'
+	end
+
+	on :mouse_move do |e|
+		time_touched = time_box.contains?(e.x, e.y) ? true : false
+		score_touched = score_box.contains?(e.x, e.y) ? true : false
+		instruction_text_touched = instruction_text.contains?(e.x, e.y) ? true : false
+		pausebox_touched = pausebox.contains?(e.x, e.y) ? true : false
+		button_yes_touched = button_yes.contains?(e.x, e.y) ? true : false
+		button_no_touched = button_no.contains?(e.x, e.y) ? true : false
+		items.each do |val|
+			items_touched = val.contains?(e.x, e.y) ? true : false
+		end
+		resume_button_touched = resume_button.contains?(e.x, e.y) ? true : false
+		resume_text_touched = resume_text.contains?(e.x, e.y) ? true : false
+		about_button_touched = about_button.contains?(e.x, e.y) ? true : false
+		power_touched = power_button.contains?(e.x, e.y) ? true : false
+		play_button2_touched = play_button2.contains?(e.x, e.y) ? true : false
+		restart_touched = restart_button.contains?(e.x, e.y) ? true : false
+	end
+
+	i = 0
+	on :mouse_down do |e|
+		button_yes_pressed = button_yes.contains?(e.x, e.y) ? true : false
+		button_no_pressed = button_no.contains?(e.x, e.y) ? true : false
+		if pausebox.contains?(e.x, e.y) then pausebox.color, pause_clicked = 'yellow', true end
+	end
+
+	on :mouse_up do |e|
+		pausebox.color, pause_clicked = 'white', false
+		if (pausebox.contains?(e.x, e.y) and pausebox.opacity > 0.1) or \
+						(resume_text.contains?(e.x, e.y) and resume_text.opacity > 0.1) or \
+						(resume_button.contains?(e.x, e.y) and resume_button.opacity > 0.1) or \
+						(play_button2.contains?(e.x, e.y) and play_button2.opacity > 0.1)
+		then
+			pause_var += 1
+			countdown = 0
+		end
+
+		exit 0 if power_button.contains?(e.x, e.y) and power_button.opacity > 0.1
+		Thread.new { system('ruby', 'stats.rb', "#{score}") } if about_button.contains?(e.x, e.y) and about_button.opacity > 0.1
+
+		score, steak, i, pause_var, prev_item = 0, 0, 0.0, 1, '' if restart_button.contains?(e.x, e.y)
+	end
+
+	zero = Text.new '0', font: 'fonts/Aller_Lt.ttf', size: 35, z: 12
+	zero.x, zero.opacity = $width/2 - zero.width/2, 0
+
+	one = Text.new '1', font: 'fonts/Aller_Lt.ttf', size: 35, z: 12
+	one.x, one.opacity = $width/2 - one.width/2, 0
+
+	two = Text.new '2', font: 'fonts/Aller_Lt.ttf', size: 35, z: 12
+	two.x, two.opacity = $width/2 - one.width/2, 0
+
+	three = Text.new '3', font: 'fonts/Aller_Lt.ttf', size: 35, z: 12
+	three.x, three.opacity = $width/2 - one.width/2, 0
+
+	beep = Sound.new 'sounds/beep.wav'
+	start_game_sound = Sound.new 'sounds/start_game.ogg'
+
+	countdown = 0
+	pressed = false
+	counter = $t.call('%s').to_i
+
+	steak = 0
+	update do
+		unless pause_var % 2 == 0
+			beep.play if countdown % $fps == 0 and !started
+			countdown += 1
+			case countdown/$fps
+				when 0
+					zero.opacity, one.opacity, two.opacity, three.opacity = 0, 0, 0, 1
+				when 1
+					zero.opacity, one.opacity, two.opacity, three.opacity = 0, 0, 1, 0
+				when 2
+					zero.opacity, one.opacity, two.opacity, three.opacity = 0, 1, 0, 0
+				when 3
+					zero.opacity, one.opacity, two.opacity, three.opacity = 1, 0, 0, 0
+				else
+					start_game_sound.play if !started
+					started = true
+			zero.opacity, one.opacity, two.opacity, three.opacity = 0, 0, 0, 0
+			end
+		else
+			started, countdown = false, 0
+		end
+
+		if pausebox_touched
+			$control.call(pausetext, '', 0.05)
+			pausebox.width += 10 if pausebox.width < pause.width + pausetext.width + 5
+		else
+			$control.call(pausetext, 'reduce', 0.1, 0)
+			pausebox.width -= 10 if pausebox.width > pause.width - 3
+		end
+
+		if about_button_touched then about_button.g -= 0.08  if about_button.g > 0.5
+			else about_button.g += 0.08 if about_button.g < 1 end
+
+		if power_touched then power_button.g -= 0.08 if power_button.g > 0.5
+			else power_button.g += 0.08 if power_button.g < 1 end
+
+		if restart_touched then restart_button.g -= 0.08 if restart_button.g > 0.5
+			else restart_button.g += 0.08 if restart_button.g < 1 end
+
+		if play_button2_touched then play_button2.g -= 0.08 if play_button2.g > 0.5
+			else play_button2.g += 0.08 if play_button2.g < 1 end
+
+		if resume_text_touched or pausebox_touched or resume_button_touched or play_button2_touched
+			resume_button.b -= 0.08 if resume_button.b > 0
+			else resume_button.b += 0.08 if resume_button.b < 1 end
+
+		timer = 45.-((i)./($fps)).to_f.round(1)
+		if timer <= 0
+			File.open('data/data', 'a+') { |file| file.puts(score) }
+			pause_var = 0
+			started = false
+			instruction_text.text, instruction_text.opacity, instruction_text.z = "Game Over. Final Score\t #{score}. Click to show stat", 1, 12
+			score, steak, prev_item = 0, 0, ''
+			i = 0.0
+		end
+
+		instruction_text_touched ? $control.call(instruction_text) : $control.call(instruction_text, '') if pause_var == 0
+		instruction_text.x = $width/2 - instruction_text.width/2
+
+		squares.each_with_index do |square, i|
+			square.y -= squares_speed[i]
+			square.rotate += squares_speed[i]
+
+			if square.y <= -square.height
+				square.width = square.height = rand(6..10)
+				squares_speed.delete_at(i)
+				squares_speed.insert(i, rand(1.0..4.0))
+				square.x, square.y, square.change_colour = rand(0..$width - square.width), $height + square.height, $generate_colour.call
+			end
+		end
+
+		if started
+			$control.call(pause_blur, 'reduce', 0.08, 0)
+			$control.call(resume_text, 'reduce', 0.08, 0)
+			$control.call(resume_button, 'reduce', 0.1, 0)
+			$control.call(power_button, 'reduce', 0.1, 0)
+			$control.call(about_button, 'reduce', 0.1, 0)
+			$control.call(play_button2, 'reduce', 0.1, 0)
+			$control.call(restart_button, 'reduce', 0.1, 0)
+
+			i += 1.0
+
+			score_text.text = "\tSCORE\t\t#{score}\t"
+			time_text.text = "\tTIME\t\t#{timer}\t"
+			instruction_text.text, instruction_text.z = 'Does this card match the previous card?', 0
+
+			score_text.x = $width - score_text.width - 5
+			score_box.x, score_box.y = score_text.x, score_text.y + 1
+			score_box.width, score_box.height = score_text.width, score_text.height
+			time_text.x = score_text.x - time_text.width - 5
+			time_box.x, time_box.y = time_text.x, time_text.y + 1
+			time_box.width, time_box.height = time_text.width, time_text.height
+
+			$control.call(correct, 'reduce', 0.08, 0)
+			$control.call(wrong, 'reduce', 0.08, 0)
+
+			time_touched ? $control.call(time_box) : $control.call(time_box, '')
+			score_touched ? $control.call(score_box) : $control.call(score_box, '')
+			instruction_text_touched ? $control.call(instruction_text) : $control.call(instruction_text, '')
+
+			pause_clicked ? $control.call(pausebox) : $control.call(pausebox, '')
+
+			items << $generate_image.call(selected_images) if items.length < 1
+			current_item = items[0].path
+
+			if counter % $fps * 3 == 0
+				counter += 1
+				selected_images = available_images.sample(rand(1..2))
+			end
+
+			items.each do |val|
+				items_touched ? $control.call(val, 'reduce', 0.08) : $control.call(val, '', 0.08)
+				val.x -= $width/15 if val.x > $width/2.0 - val.width/2.0
+			end
+
+			if button_yes_touched
+				$control.call(button_yes)
+	 			yes_text.b += 0.15 if yes_text.b < 1
+			else
+				$control.call(button_yes, '')
+				yes_text.color = [0.8, 0.8, 0.3, 1]
+			end
+
+			if button_no_touched
+				$control.call(button_no)
+	 			no_text.b += 0.15 if yes_text.b < 1
+			else
+				$control.call(button_no, '')
+				no_text.color = [0.8, 0.8, 0.3, 1]
+			end
+
+			if button_yes_pressed
+				pressed = true
+				button_yes_pressed, button_no_pressed = false, false
+				yes_text.color = 'red'
+				if prev_item == current_item
+					steak += 1
+					correct.opacity = 1
+					score += 1 * steak
+					sound_correct.play
+				else
+					steak = 0
+					wrong.opacity = 1
+					sound_wrong.play
+					score -= 1 * steak
+				end
+			end
+
+			if button_no_pressed
+				pressed = true
+				button_yes_pressed, button_no_pressed = false, false
+				no_text.color = 'red'
+				unless prev_item == current_item
+					steak += 1
+					correct.opacity = 1
+					score += 1 * steak
+					sound_correct.play
+				else
+					steak = 0
+					wrong.opacity = 1
+					sound_wrong.play
+					score -= 1 * steak
+				end
+			end
+
+			if pressed
+				items.each do |val|
+					val.x -= $width/10.0
+					val.opacity -= 0.2
+					if val.x <= -val.width
+						pressed = false
+						prev_item = val.path
+						val.remove
+						items.pop
+					end
+				end
+			end
+			else
+				resume_button_touched ? $control.call(resume_button) : $control.call(resume_button, '')
+				resume_text_touched ? $control.call(resume_text) : $control.call(resume_text, '')
+				$control.call(power_button, '')
+				$control.call(about_button, '')
+				$control.call(play_button2, '')
+				$control.call(restart_button, '')
+				$control.call(pause_blur, '', 0.05, 0.65, 0.7)
+		end
+		counter += 1
+	end
+	Window.show
+end
+main
---
+++ stats.rb
@@ -0,0 +1,164 @@
+#!/usr/bin/ruby
+require 'ruby2d'
+STDOUT.sync = true
+on :key_down do |k| exit 0 if k.key == 'escape' end
+
+module Ruby2D
+	def get_colour() [self.r, self.g, self.b, self.opacity] end
+end
+
+def main
+	$width, $height, $fps = 640, 480, 50
+	set title: 'Speed Match Statistics', width: $width, height: $height, fps_cap: $fps, background: 'white'
+
+	$generate_colour = -> {
+		colour = ''
+		6.times do colour += [('0'..'9').to_a.sample, ('a'..'f').to_a.sample].sample end
+		"##{colour}"
+	}
+
+	$control = ->(object, type='reduce', val=0.05, threshold=0.6) {
+		if type.start_with?('r')
+			object.opacity -= val if object.opacity > threshold
+		else
+			object.opacity += val if object.opacity < 1
+		end
+	}
+
+	stat_bg = Image.new 'images/bg_stat_window.png', width: $width, height: $height
+	stat_bg.opacity = 0.2
+
+	read_score = File.readlines('data/data').last(7).map(&:to_i)
+	last_score = read_score[-2]
+	score = read_score[-1]
+	# puts read_score.max
+
+	if score < 250 then iqtype = 'Very Low'
+		elsif score < 500 then iqtype = 'Low'
+		elsif score < 750 then iqtype = 'Average'
+		elsif score < 900 then iqtype = 'Good'
+		else iqtype = 'Excellent'
+	end
+
+	game_details = <<EOF
+Speed Match is a game where you have to decide if the
+previous image matches the current image.
+The images are often reffered to as cards.
+
+How to play: Just tap yes if the previous card
+match the old one, or else, just tap no.
+
+Benefit: Playing this game may increase your
+brain's flexibility.
+
+Scores: The graph on the right side shows
+the details.
+
+Your score:
+You have scored #{score}.
+
+Your past score: #{last_score}.
+
+Your last 7 scores:
+#{read_score}
+EOF
+	game_details = game_details.split("\n")
+	game_details.each_with_index do |l, i|
+		Text.new l, font: 'fonts/Aller_Lt.ttf', x: 5, y: i * 20, size: 20, color: 'black'
+	end
+
+	note = Text.new 'NOTE: Neither this game nor these score statistics are based on real life mental test.',
+		font: 'fonts/Aller_Lt.ttf', color: 'red', x: 5, size: 15
+	note.y = $height - note.height - 5
+
+	triangles = []
+	j = 10
+	20.step(260, 60) do |i|
+		triangles << Triangle.new(
+				x1: $height - j + 50 + 60, y1: 0 + i,
+				x2: $height - j + 100 + 60, y2: 350,
+				x3: $height - j + 60, y3: 350,
+ 				color: $generate_colour.call)
+		j += 50
+	end
+	triangles.reverse!
+
+	case iqtype
+		when 'Very Low' then you_in = 0
+		when 'Low' then you_in = 1
+		when 'Average' then you_in = 2
+		when 'Good' then you_in = 3
+		when 'Excellent' then you_in = 4
+	end
+
+	a_line = Line.new color: 'black', x1: triangles[0].x3, x2: triangles[-1].x2, y1: triangles[0].y2 + 10, y2: triangles[-1].y2 + 10
+
+	very_low_text = Text.new 'Very Low', font: 'fonts/Aller_Lt.ttf', color: triangles[0].get_colour, size: 15
+	very_low_text.x, very_low_text.y = triangles[0].x1 - very_low_text.width/2, triangles[0].y1 - very_low_text.height
+
+	low_text = Text.new 'Low', font: 'fonts/Aller_Lt.ttf', color: triangles[1].get_colour, size: 15
+	low_text.x, low_text.y = triangles[1].x1 - low_text.width/2, triangles[1].y1 - low_text.height
+
+	average_text = Text.new 'Average', font: 'fonts/Aller_Lt.ttf', color: triangles[2].get_colour, size: 15
+	average_text.x, average_text.y = triangles[2].x1 - average_text.width/2, triangles[2].y1 - average_text.height
+
+	good_text = Text.new 'Good', font: 'fonts/Aller_Lt.ttf', color: triangles[3].get_colour, size: 15
+	good_text.x, good_text.y = triangles[3].x1 - good_text.width/2, triangles[3].y1 - good_text.height
+
+	excellent_text = Text.new 'Excellent', font: 'fonts/Aller_Lt.ttf', color: triangles[4].get_colour, size: 15
+	excellent_text.x, excellent_text.y = triangles[4].x1 - excellent_text.width/2, triangles[4].y1 - excellent_text.height
+
+	you = Text.new 'YOU', font: 'fonts/Aller_Lt.ttf', size: 15
+	you.x = triangles[you_in].x1 - you.width/2
+	you.y = triangles[you_in].y1/2 + triangles[you_in].y2/2 - you.height/2
+
+	details_raw = <<EOF
+					 VERY LOW: You have to improve yourself.
+ 					LOW: You need to make some improvements.
+ 					AVERAGE: Your brain's performance is normal.
+ 					GOOD: Your brain works very fast.
+ 					EXCELLENT: Your brain is highly flexible, ultra fast.
+EOF
+		details_raw = details_raw.split("\n")
+
+	height = 0
+	details_info = []
+	details_raw.each_with_index do |c, i|
+		details = Text.new(c, font: 'fonts/Aller_Lt.ttf', color: triangles[i].get_colour, x: a_line.x1 - 25, y: a_line.y1 + 5 + height, size: 11)
+		details_info << details
+		height += details.height
+	end
+
+	hovered_on_triangle, details_hover = nil, false
+
+	on :mouse_move do |e|
+		if very_low_text.contains?(e.x, e.y) then very_low_text.color = [0,0,0,1]
+			elsif very_low_text.get_colour == [0,0,0,1] then very_low_text.color = $generate_colour.call end
+
+		if low_text.contains?(e.x, e.y) then low_text.color = [0,0,0,1]
+			elsif low_text.get_colour == [0,0,0,1] then low_text.color = $generate_colour.call end
+
+		if average_text.contains?(e.x, e.y) then average_text.color = [0,0,0,1]
+			elsif average_text.get_colour == [0,0,0,1] then average_text.color = $generate_colour.call end
+
+		if good_text.contains?(e.x, e.y) then good_text.color = [0,0,0,1]
+			elsif good_text.get_colour == [0,0,0,1] then good_text.color = $generate_colour.call end
+
+		if excellent_text.contains?(e.x, e.y) then excellent_text.color = [0,0,0,1]
+			elsif excellent_text.get_colour == [0,0,0,1] then excellent_text.color = $generate_colour.call end
+	end
+
+	update do
+		triangles[0].color = very_low_text.get_colour
+		triangles[1].color = low_text.get_colour
+		triangles[2].color = average_text.get_colour
+		triangles[3].color = good_text.get_colour
+		triangles[4].color = excellent_text.get_colour
+
+		details_info.each_with_index do |val, i|
+			val.color = triangles[i].get_colour
+		end
+	end
+	show
+end
+main()
